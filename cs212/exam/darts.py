7b8743d158a8d156e7eab8c3d454beaf6685da4a
Derek Henscheid <derek-henscheid@uiowa.edu>
2012-08-24 17:35:15 -0500

    almost done with the darts

--- cs212/exam/darts.py
+++ cs212/exam/darts.py
@@ -44,7 +44,7 @@ a solution that way.  If not, try T19 first, and so on. At first I thought
 I would need three passes: first try to solve with one dart, then with two,
 then with three.  But I realized that if we include 0 as a possible dart
 value, and always try the 0 first, then we get the effect of having three
-passes, but we only have to code one pass.  So I creted ordered_points as
+passes, but we only have to code one pass.  So I created ordered_points as
 a list of all possible scores that a single dart can achieve, with 0 first,
 and then descending: [0, 60, 57, ..., 1].  I iterate dart1 and dart2 over
 that; then dart3 must be whatever is left over to add up to total.  If
@@ -56,8 +56,48 @@ we must choose a double for the last dart, but for the others I prefer the
 easiest targets first: 'S' is easiest, then 'T', then 'D'.
 """
 
+TARGETS = dict(reduce(lambda x, y: dict(x.items() + y.items()), [{('S%d' % x): x, ('D%d' % x):x*2, ('T%d' % x):x*3} for x in range(1,21)]).items() + {'DB': 50, 'SB': 25, 'OFF': 0}.items())
+POSSIBLE_SCORES = [0] + sorted(set(TARGETS.values()), reverse=True)
+SECTIONS = {'20': ['1', '5'], '1': ['20', '18'], '18': ['1', '4'],
+            '4': ['18', '13'], '13': ['4', '6'], '6': ['13', '10'],
+            '10': ['6', '15'], '15': ['10', '2'], '2': ['15', '17'],
+            '17': ['2', '3'], '3': ['17', '19'], '19': ['3', '7'],
+            '7': ['19', '16'], '16': ['7', '8'], '8': ['16', '11'],
+            '11': ['8', '14'], '14': ['11', '9'], '9': ['14', '12'],
+            '12': ['9', '5'], '5': ['12', '20']}
+
+def name(d, needs_double=False):
+    if not needs_double:
+        if d <= 20:
+            return 'S%d' % d
+        elif d % 3 is 0 and d <= 60:
+            return 'T%d' % (d / 3)
+        elif d % 2 is 0 and d <= 40:
+            return 'D%d' % (d / 2)
+        elif d == 25:
+            return 'SB'
+        elif d == 50:
+            return 'DB'
+        else:
+            return ''
+    else:
+        if d % 2 is 0:
+            return 'D%d' % (d / 2)
+        else:
+            return name(d)
 
 def double_out(total):
+    scores = []
+    for score1 in POSSIBLE_SCORES:
+        for score2 in POSSIBLE_SCORES:
+            score3 = (total - score1 - score2)
+            if score3 in POSSIBLE_SCORES:
+                scores = filter(lambda x: x != 0, [score1, score2, score3])
+                darts = sorted(map(name, scores), reverse=True)
+                if darts[-1][0] == 'D': return darts
+
+    if not len(scores): return None
+
     """Return a shortest possible list of targets that add to total,
     where the length <= 3 and the final element is a double.
     If there is no solution, return None."""
@@ -121,14 +161,52 @@ is large; also, it is always possible to miss a double, and thus there is
 no guarantee that the game will end in a finite number of moves.
 """
 
+def neighbors(target):
+    if target == 'DB' or target == 'SB':
+        return SECTIONS.keys()
+    return SECTIONS[target[1:]]
 
 def outcome(target, miss):
+    miss = miss * 0.2 if target[0] == 'S' else 3 * miss if target == 'DB' else miss
+    hit = 1 - miss
+    around_me = neighbors(target)
+    h_miss = miss * (1.0/len(around_me))
+    distribution = {target: hit ** 2}
+
+    for neighbor in around_me:
+        side = 'S' if target == 'DB' or target == 'SB' else target[0]
+        if target == 'SB' or target == 'DB':
+            distribution[target] = hit
+            if target == 'SB':
+                distribution['DB'] = 0.25 * miss
+                distribution[side + neighbor] = 0.75 * h_miss
+            else:
+                distribution['SB'] = (1.0 / 3) * miss
+                distribution[side + neighbor] = (2.0 / 3) * h_miss
+        else:
+            distribution[side + neighbor] = hit * h_miss
+
+            if side == 'T':
+                distribution['S' + neighbor] = miss * h_miss
+                distribution['S' + target[1:]] = hit * miss
+            elif side == 'S':
+                distribution['T' + neighbor] = miss * 0.5 * h_miss
+                distribution['D' + neighbor] = miss * 0.5 * h_miss
+                distribution['T' + target[1:]] = miss * 0.5 * hit
+                distribution['D' + target[1:]] = miss * 0.5 * hit
+            else:
+                distribution['OFF'] = miss * 0.5
+                distribution['S' + neighbor] = miss * 0.5 * h_miss
+                distribution['S' + target[1:]] = miss * 0.5 * hit
+
+
     "Return a probability distribution of [(target, probability)] pairs."
-    #your code here
+    return distribution
 
 def best_target(miss):
     "Return the target that maximizes the expected score."
     #your code here
+    return max([(sum(map(lambda x: TARGETS[x[0]] * x[1], outcome(target, miss).iteritems())), target) for target in TARGETS.keys() if target != 'OFF'])[1]
 
 def same_outcome(dict1, dict2):
     "Two states are the same if all corresponding sets of locs are the same."
@@ -150,3 +228,7 @@ def test_darts2():
              'S19': 0.016, 'S18': 0.016, 'S13': 0.016, 'S12': 0.016, 'S11': 0.016,
              'S10': 0.016, 'S17': 0.016, 'S16': 0.016, 'S15': 0.016, 'S14': 0.016,
              'S7': 0.016, 'SB': 0.64}))
+
+
+print(outcome('SB', 0.2))
+print test_darts2()
